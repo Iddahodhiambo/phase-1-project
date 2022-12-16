6e22f2a6d4d3d996b5f2ea35fa68d58e6c7e166a
Iddahodhiambo <iddahawuor@yahoo.com>
2022-12-16 17:39:30 +0300

    displaying books details

--- src/index.js
+++ src/index.js
@@ -13,4 +13,33 @@ fetch("https://anapioficeandfire.com/api/books").then((response) => response.jso
 }
 
 displayBooksName()
+//display books details
 
+const booksDetailContainer = document.getElementById('booksDetails')
+function displayBooksDetails(e) {
+fetch("https://anapioficeandfire.com/api/books").then((response) => response.json()).then((data)=> {
+    data.map(book => {
+        if(e.target.innerHTML === book.name) {
+            const books = `
+            <p><b>NAME</b>: ${book.name}</p>
+            <p><b>ISBN</b>: ${book.isbn}</p>
+            <p><b>AUTHORS</b>: ${book.authors}</p>
+            <p><b>NUMBER OF PAGES</b>: ${book.numberOfPages}</p>
+            <p><b>PUBLISHER</b>: ${book.publisher}</p>
+            <p><b>COUNTRY</b>: ${book.country}</p>
+            <p><b>MEDIA TYPE</b>: ${book.mediaType}</p>
+            <p><b>RELEASED </b>: ${book.released}</p>
+            
+             `
+      booksDetailContainer.innerHTML = ""
+booksDetailContainer.insertAdjacentHTML("afterbegin",books)
+
+            }
+
+        })
+    })
+
+
+}
+booksList.addEventListener('click', displayBooksDetails)
+    
